812158d00b10c652089dbd514b36cf6f4396d1aa
AngelC26 <crisostomoperezangel2@gmail.com>
2025-10-31 22:28:06 -0400

    datos parada

--- src/main/java/com/example/sistemagestionrutasaced/logico/Parada.java
+++ src/main/java/com/example/sistemagestionrutasaced/logico/Parada.java
@@ -1,8 +1,22 @@
 package com.example.sistemagestionrutasaced.logico;
 
-public class Parada
+public class Parada {
+
+    private String id;
+    private String nombre;
 
 
+    public Parada(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
 
-{
+    public String getId() {
+        return id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+
 }
